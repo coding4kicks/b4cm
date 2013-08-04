9d85d49c4d89cfb26f38667099eddd1b9b504d3b
Poof Young <poof@coding4kicks.com>
2013-08-03 23:29:10 -0700

    Initial Commit - Yeoman Angular scaffolding with Bootstrap and Compass, plug .gitignore for Mac and Vim.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('b4cmApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
