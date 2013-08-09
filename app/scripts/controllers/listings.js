3b990c69f4a272b3e3c6bc1126ed1b007231a97b
Poof Young <poof@coding4kicks.com>
2013-08-09 15:04:23 -0700

    Remove awesome things from main and listings controllers.

--- app/scripts/controllers/listings.js
+++ app/scripts/controllers/listings.js
@@ -2,11 +2,6 @@
 
 angular.module('b4cmApp')
   .controller('ListingsCtrl', function ($scope, $location) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     $scope.findSpot = function() {
       $location.path("/spot");
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,15 +2,11 @@
 
 angular.module('b4cmApp')
   .controller('MainCtrl', function ($scope, $location) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     $scope.findListings = function() {
       $location.path("/listings");
       // http://www.yearofmoo.com/2012/10/ ... apply-digest-and-phase
       if(!$scope.$$phase) { $scope.$apply(); }
     };
+
   });
