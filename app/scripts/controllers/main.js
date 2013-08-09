3df20f78e4d163704c2aa922db5a328baaa0f800
Poof Young <poof@coding4kicks.com>
2013-08-09 14:13:20 -0700

    Create findListings function in main controller and set ng-clicks for findListings in main view.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('b4cmApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.findListings = function() {
+      alert('here');
+      //$location.path("#/listings");
+      //$scope.$apply();
+    };
   });
