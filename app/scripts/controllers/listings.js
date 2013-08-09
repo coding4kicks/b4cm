f722361e07838692c9798d88e2a2117d0b1cc1c1
Poof Young <poof@coding4kicks.com>
2013-08-09 14:55:27 -0700

    Create findSpot function to redirect to spot details page.

--- app/scripts/controllers/listings.js
+++ app/scripts/controllers/listings.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('b4cmApp')
-  .controller('ListingsCtrl', function ($scope) {
+  .controller('ListingsCtrl', function ($scope, $location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.findSpot = function() {
+      $location.path("/spot");
+      // http://www.yearofmoo.com/2012/10/ ... apply-digest-and-phase
+      if(!$scope.$$phase) { $scope.$apply(); }
+    };
+
   });
