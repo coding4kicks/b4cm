4a725bfbb96e9e46e675bcd1f10ba04bb6209aaf
Poof Young <poof@coding4kicks.com>
2013-08-13 17:01:38 -0700

    Temporarily remove google maps directive.

--- app/scripts/controllers/listings.js
+++ app/scripts/controllers/listings.js
@@ -9,4 +9,13 @@ angular.module('b4cmApp')
       if(!$scope.$$phase) { $scope.$apply(); }
     };
 
+    //angular.extend($scope, {
+	  //  center: {
+		//    latitude: 0, // initial map center latitude
+		//    longitude: 0, // initial map center longitude
+	  //  },
+	  //  markers: [], // an array of markers,
+	  //  zoom: 8, // the zoom level
+    //});
+
   });
