c40efaaba8328bf28d41254660aee90272c0c213
Poof Young <poof@coding4kicks.com>
2013-08-20 10:39:03 -0700

    Add new elements to spot page title bar: current status, map and ad.

--- app/scripts/controllers/spot.js
+++ app/scripts/controllers/spot.js
@@ -2,9 +2,49 @@
 
 angular.module('b4cmApp')
   .controller('SpotCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  // Enable the new Google Maps visuals until it gets enabled by default.
+  // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
+  google.maps.visualRefresh = true;
+
+  angular.extend($scope, {
+    
+    position: {
+      coords: {
+      latitude: 37.447365,
+      longitude: -122.160248
+      }
+    },
+    
+    /** the initial center of the map */
+    centerProperty: {
+      latitude: 37.447365,
+      longitude: -122.160248
+    },
+      
+    /** the initial zoom level of the map */
+    zoomProperty: 14,
+      
+    /** list of markers to put in the map */
+    markersProperty: [
+      { latitude: 37.441838 ,
+        longitude: -122.161675,
+        infoWindow: "Philz Coffee",
+        icon : {url: "../images/marker-icon1.png"}
+      },
+    ],
+      
+    // These 2 properties will be set when clicking on the map
+    clickedLatitudeProperty: null,	
+    clickedLongitudeProperty: null,
+      
+    eventsProperty: {
+      click: function (mapModel, eventName, originalEventArgs) {	
+        // 'this' is the directive's scope
+        $log.log("user defined event on map directive with scope", this);
+        $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
+      }
+    }
+  });
+    
   });
