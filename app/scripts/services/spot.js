8c2d402ed6eb0fffc6bfd1639937da46473e2a79
Poof Young <poof@coding4kicks.com>
2013-08-21 14:38:28 -0700

    Begin construction of fake spot JSON object.

--- app/scripts/services/spot.js
+++ app/scripts/services/spot.js
@@ -5,7 +5,50 @@ angular.module('b4cmApp')
     // Service logic
     // ...
 
-    var spot = {};
+    var fakeSpot = {
+      'id': '123',
+      'name': 'Philz Coffee',
+      'yelp_id': 'philz-coffee-palo-alto2',
+      'rating': 3.5,
+      'wifi': true,
+      'type': {
+        'food': 5,
+        'study': 8,
+        'social': 1
+        },
+      'location': {
+        'address': '101 Forest Ave',
+        'city': 'Palo Alto',
+        'state_code': 'CA',
+        'postal_code': '94301',
+        'latitude': 37.441838,
+        'longitude': -122.161675,
+        'geohash': '9q9jh0hdd8kz'
+        },
+        'reviews': [
+          {
+            'author': {
+              'id': '12345',
+              'name': 'Steve S.',
+              'pic': '../images/peeps1.jpg'
+            },
+            'date': 'Tuesday, June 11th, 2013',
+            'rating': 4.0,
+            'writeup': 'Tesora = King of Coffee',
+            'type': {
+              'food': 1,
+              'study': 1,
+              'social': 0
+            }
+          },
+          {
+          },
+          {
+          }
+        ],
+        'crowdfactor': {
+        }
+      };
 
     // Public API here
     return {
