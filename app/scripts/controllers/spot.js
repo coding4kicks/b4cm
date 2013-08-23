879711ef899fa09afe5d550658ce77aacd344cb4
Poof Young <poof@coding4kicks.com>
2013-08-23 01:30:26 -0700

    Refactor google maps initialization into helper function.

--- app/scripts/controllers/spot.js
+++ app/scripts/controllers/spot.js
@@ -34,53 +34,55 @@ angular.module('b4cmApp')
     $scope.show_marker = _initializeShowMarkerMatrix();
     $scope.current_marker = {'day': '', 'hour': '', 'meridiem': ''}; // Currently visible marker position
 
-    /**
-     * GOOGLE MAPS
-     */
+    // Initialize google maps parameters
+    _initializeGoogleMaps($scope);
+   // /**
+   //  * GOOGLE MAPS
+   //  */
 
-    // Enable the new Google Maps visuals until it gets enabled by default.
-    // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
-    // Add test for existance so doesn't blow up unit tests
-    if (typeof google !== "undefined") {
-      google.maps.visualRefresh = true;
-    }
-    
-    $scope.position = {
-      'coords': {
-        'latitude': $scope.spot.location.latitude,
-        'longitude': $scope.spot.location.longitude
-      }
-    };
+   // // Enable the new Google Maps visuals until it gets enabled by default.
+   // // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
+   // // Add test for existance so doesn't blow up unit tests
+   // if (typeof google !== "undefined") {
+   //   google.maps.visualRefresh = true;
+   // }
+   // 
+   // $scope.position = {
+   //   'coords': {
+   //     'latitude': $scope.spot.location.latitude,
+   //     'longitude': $scope.spot.location.longitude
+   //   }
+   // };
 
-    /** the initial center of the map */
-    $scope.centerProperty = {
-      'latitude': $scope.spot.location.latitude,
-      'longitude': $scope.spot.location.longitude
-    };
+   // /** the initial center of the map */
+   // $scope.centerProperty = {
+   //   'latitude': $scope.spot.location.latitude,
+   //   'longitude': $scope.spot.location.longitude
+   // };
 
-    /** the initial zoom level of the map */
-    $scope.zoomProperty = 14;
+   // /** the initial zoom level of the map */
+   // $scope.zoomProperty = 14;
 
-    /** list of markers to put in the map */
-    $scope.markersProperty = [
-      { 'latitude': $scope.spot.location.latitude,
-        'longitude': $scope.spot.location.longitude,
-        'infoWindow': $scope.spot.name,
-        'icon' : {'url': "../images/marker-icon1.png"}
-      },
-    ];
-        
-    // These 2 properties will be set when clicking on the map
-    $scope.clickedLatitudeProperty = null;
-    $scope.clickedLongitudeProperty = null;
-        
-    $scope.eventsProperty = {
-      'click': function (mapModel, eventName, originalEventArgs) {	
-        // 'this' is the directive's scope
-        $log.log("user defined event on map directive with scope", this);
-        $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
-      }
-    };
+   // /** list of markers to put in the map */
+   // $scope.markersProperty = [
+   //   { 'latitude': $scope.spot.location.latitude,
+   //     'longitude': $scope.spot.location.longitude,
+   //     'infoWindow': $scope.spot.name,
+   //     'icon' : {'url': "../images/marker-icon1.png"}
+   //   },
+   // ];
+   //     
+   // // These 2 properties will be set when clicking on the map
+   // $scope.clickedLatitudeProperty = null;
+   // $scope.clickedLongitudeProperty = null;
+   //     
+   // $scope.eventsProperty = {
+   //   'click': function (mapModel, eventName, originalEventArgs) {	
+   //     // 'this' is the directive's scope
+   //     $log.log("user defined event on map directive with scope", this);
+   //     $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
+   //   }
+   // };
 
     /**
      * @name addWatch
@@ -315,5 +317,43 @@ function _initializeShowMarkerMatrix() {
   return show_marker;
 }
 
-
-
+// Initialize google maps parameters
+function _initializeGoogleMaps($scope) {
+  // Enable the new Google Maps visuals until it gets enabled by default.
+  // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
+  // Add test for existance so doesn't blow up unit tests
+  if (typeof google !== "undefined") {
+    google.maps.visualRefresh = true;
+  }
+  $scope.position = {
+    'coords': {
+      'latitude': $scope.spot.location.latitude,
+      'longitude': $scope.spot.location.longitude
+    }
+  };
+  /** the initial center of the map */
+  $scope.centerProperty = {
+    'latitude': $scope.spot.location.latitude,
+    'longitude': $scope.spot.location.longitude
+  };
+  /** the initial zoom level of the map */
+  $scope.zoomProperty = 14;
+  /** list of markers to put in the map */
+  $scope.markersProperty = [
+    { 'latitude': $scope.spot.location.latitude,
+      'longitude': $scope.spot.location.longitude,
+      'infoWindow': $scope.spot.name,
+      'icon' : {'url': "../images/marker-icon1.png"}
+    },
+  ];   
+  // These 2 properties will be set when clicking on the map
+  $scope.clickedLatitudeProperty = null;
+  $scope.clickedLongitudeProperty = null;   
+  $scope.eventsProperty = {
+    'click': function (mapModel, eventName, originalEventArgs) {	
+      // 'this' is the directive's scope
+      $log.log("user defined event on map directive with scope", this);
+      $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
+    }
+  };
+}
