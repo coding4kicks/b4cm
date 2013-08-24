1b226b65c6f154bbbe8886916e91a7f65400ab45
Poof Young <poof@coding4kicks.com>
2013-08-23 22:26:11 -0700

    Added functionality for current time, plus 2 hours and plus 4 hours generation to listings page crowd status bars.

--- app/scripts/controllers/listings.js
+++ app/scripts/controllers/listings.js
@@ -19,6 +19,29 @@ angular.module('b4cmApp')
           spot_obj.stars = _calculateStars(spot_obj.rating);
       $scope.spots.push(spot_obj);
     });
+
+    // Calculate current time, +2 hours, and +4 hours
+    var current_date = new Date(),
+        current_hour24 = current_date.getHours(),
+        plus2_hour24 = current_hour24 + 2,
+        plus4_hour24 = current_hour24 + 4,
+        current_hour = current_hour24 % 12,
+        plus2_hour = plus2_hour24 % 12,
+        plus4_hour = plus4_hour24 % 12,
+        current_meridiem = (current_hour24 - 12 < 0) ? 'am' : 'pm',
+        plus2_meridiem = (plus2_hour24 - 12 < 0 || plus2_hour24 > 23) ? 'am' : 'pm',
+        plus4_meridiem = (plus4_hour24 - 12 < 0 || plus4_hour24 > 23) ? 'am' : 'pm';
+    if (current_hour === 0) {current_hour = 12}
+    else if (plus2_hour === 0) {plus2_hour = 12}
+    else if (plus4_hour === 0) {plus4_hour = 12}
+    console.log(current_hour);
+    console.log(plus2_hour);
+    console.log(plus4_hour);
+    $scope.current_time = current_hour + current_meridiem;
+    $scope.plus2_time = plus2_hour + plus2_meridiem;
+    $scope.plus4_time = plus4_hour + plus4_meridiem;
+
+
     //console.log(spots);
     
 
