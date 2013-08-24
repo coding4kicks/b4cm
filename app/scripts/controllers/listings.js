0a95d150c1b0f5e92ea42ba1d542fc810c760c23
Poof Young <poof@coding4kicks.com>
2013-08-24 13:23:47 -0700

    Add function to set status for watch boxes on listings page. Bug that all are same.

--- app/scripts/controllers/listings.js
+++ app/scripts/controllers/listings.js
@@ -17,45 +17,15 @@ angular.module('b4cmApp')
         current_date = new Date(),
         current_time = timeInfo(current_date),
         plus1_time = timeInfo(new Date(current_date.getTime() + 1 * hour)),
-        plus2_time = timeInfo(new Date(current_date.getTime() + 1 * hour)),
-        plus3_time = timeInfo(new Date(current_date.getTime() + 1 * hour)),
-        plus4_time = timeInfo(new Date(current_date.getTime() + 1 * hour));
+        plus2_time = timeInfo(new Date(current_date.getTime() + 2 * hour)),
+        plus3_time = timeInfo(new Date(current_date.getTime() + 3 * hour)),
+        plus4_time = timeInfo(new Date(current_date.getTime() + 4 * hour)),
+        times = [current_time, plus1_time, plus2_time, plus3_time, plus4_time];
 
-    $scope.current_time = current_time.getLabel();
-    $scope.plus2_time = current_time.getLabel();
-    $scope.plus4_time = current_time.getLabel();
+    $scope.current_time = current_time.getTimeLabel();
+    $scope.plus2_time = plus2_time.getTimeLabel();
+    $scope.plus4_time = plus4_time.getTimeLabel();
 
-   // console.log(plus1_time.getLabel());
-
-   // var cur_time = timeInfo(new Date());
-   // console.log(cur_time.getLabel());
-   // // Calculate current time, +2 hours, and +4 hours
-   // var current_date = new Date(),
-   //     WEEKDAY = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-   //     current_day = WEEKDAY[current_date.getDay()],
-   //     current_hour24 = current_date.getHours(),
-   //     plus1_hour24 = current_hour24 + 1,
-   //     plus2_hour24 = current_hour24 + 2,
-   //     plus3_hour24 = current_hour24 + 3,
-   //     plus4_hour24 = current_hour24 + 4,
-   //     current_hour = current_hour24 % 12,
-   //     plus1_hour = plus1_hour24 % 12,
-   //     plus2_hour = plus2_hour24 % 12,
-   //     plus3_hour = plus3_hour24 % 12,
-   //     plus4_hour = plus4_hour24 % 12,
-   //     current_meridiem = (current_hour24 - 12 < 0) ? 'am' : 'pm',
-   //     plus1_meridiem = (plus1_hour24 - 12 < 0 || plus1_hour24 > 23) ? 'am' : 'pm',
-   //     plus2_meridiem = (plus2_hour24 - 12 < 0 || plus1_hour24 > 23) ? 'am' : 'pm',
-   //     plus3_meridiem = (plus3_hour24 - 12 < 0 || plus3_hour24 > 23) ? 'am' : 'pm',
-   //     plus4_meridiem = (plus4_hour24 - 12 < 0 || plus4_hour24 > 23) ? 'am' : 'pm';
-   // if (current_hour === 0) {current_hour = 12}
-   // else if (plus2_hour === 0) {plus2_hour = 12}
-   // else if (plus4_hour === 0) {plus4_hour = 12}
-   // $scope.current_time = current_hour + current_meridiem;
-   // $scope.plus1_time = plus1_hour + plus1_meridiem;
-   // $scope.plus2_time = plus2_hour + plus2_meridiem;
-   // $scope.plus3_time = plus3_hour + plus3_meridiem;
-   // $scope.plus4_time = plus4_hour + plus4_meridiem;
 
     var CFLABELS = ['Empty', 'Few', 'Average', 'Crowded', 'Herd'];
     $scope.spots = [];
@@ -70,16 +40,13 @@ angular.module('b4cmApp')
           //count2 = spot_obj.crowdfactor.day[current_day.toLowerCase()][$scope.plus2_time].count,
           //score2 = spot_obj.crowdfactor.day[current_day.toLowerCase()][$scope.plus2_time].score;
       
-          // Calculate star rating for spot
-          spot_obj.stars = _calculateStars(spot_obj.rating);
+      // Calculate star rating for spot
+      spot_obj.stars = _calculateStars(spot_obj.rating);
 
-          // Calculate crowd status for crowd watch bar
-          //spot_obj.cf_status_label = 
-          //spot_obj.cf_status =
-          // spot_obj.cf_status1 =
-          //spot_obj.cf_status2 =
-          //spot_obj.cf_status3 =
-          //spot_obj.cf_status4 =
+      // Calculate crowd status for crowd watch bar
+      spot_obj.cf_status_label = ''//_calculateStatus(spot_obj, current_time);
+      spot_obj.cf_status_boxes = _calculateBoxLabels(spot_obj, times)
+      console.log(spot_obj.cf_status_boxes);
       $scope.spots.push(spot_obj);
     });
 
@@ -114,14 +81,27 @@ function timeInfo(date) {
       hour24 = date.getHours(),
       hour = hour24 % 12,
       meridiem = (hour24 - 12 < 0) ? 'am' : 'pm',
-      label = '';
+      timeLabel = '';
   if (hour === 0) {hour = 12};
-  label = hour + meridiem;
+  timeLabel = hour + meridiem;
 
   return {
-    getLabel: function() {return label},
-    getDay: function() {return day}
+    getTimeLabel: function() {return timeLabel;},
+    getDay: function() {return day;}
   }
+}
+
+function _calculateBoxLabels(spot_obj, times) {
+  var cf_labels = [];
+  times.forEach(function(time) {
+    var day = spot_obj.crowdfactor.day[time.getDay().toLowerCase()],
+        count = day[time.getTimeLabel()].count,
+        score = day[time.getTimeLabel()].score,
+        cf_score = score / count;
+    if(score === -1){cf_score = -1};
+    cf_labels.push(_setStatus(cf_score));
+  });
+  return cf_labels;
 }
 
 
