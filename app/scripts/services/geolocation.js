2a499e7dfdd00ede2ba4cceef3e1a0b8f2ad3bcd
Poof Young <poof@coding4kicks.com>
2013-08-28 14:02:17 -0700

    Add service files for geoloaction.

---
+++ app/scripts/services/geolocation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('b4cmApp')
+  .factory('geolocation', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
