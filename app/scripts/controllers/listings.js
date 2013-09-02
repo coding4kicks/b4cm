d012d1840585ee358664c797db7072087a17073f
Poof Young <poof@coding4kicks.com>
2013-09-01 18:02:20 -0700

    New session.

--- app/scripts/controllers/listings.js
+++ app/scripts/controllers/listings.js
@@ -49,6 +49,7 @@ angular.module('b4cmApp')
     $scope.zoomProperty = 14;
   
     listings.get(searchLocation, spotType).then(function(idList) {
+      $scope.numReturns = idList.length;
       if (idList.length === 0) {$scope.noSpots = true;}
       else {
         // Get and format spot info for each spot in returned id list.
