dcf3528b115fb89f3b0bb4d942a56af562f0580e
Poof Young <poof@coding4kicks.com>
2013-09-02 21:48:40 -0700

    Remove awesome things.

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -2,9 +2,4 @@
 
 angular.module('b4cmApp')
   .controller('SignupCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
