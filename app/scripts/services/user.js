5c16c43f26057dd96aef2c06df4dd2ca4d932e70
Poof Young <poof@coding4kicks.com>
2013-09-02 18:40:28 -0700

    Add stubs for public api functions.  Add this.user rerference on login in auth.

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -4,27 +4,47 @@ angular.module('b4cmApp')
   .factory('user', function () {
 
     var ref = new Firebase('https://crowd-data.firebaseIO.com'),
-        auth = new FirebaseSimpleLogin(ref, function(error, user) {
+        user = {};
+
+    var auth = new FirebaseSimpleLogin(ref, function(error, user) {
       if (error) {
         // an error occurred while attempting login
         console.log(error);
       } else if (user) {
         // user authenticated with Firebase
+        this.user = user;
         console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
       } else {
         // user is logged out
       }
     });
 
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
     // Public API here
     return {
-      someMethod: function () {
+      signUp: function (email, password) {
+        auth.createUser(email, password, function(error, user) {
+          if (!error) {
+            console.log('User Id: ' + user.id + ', Email: ' + user.email);
+          }
+        });
         return meaningOfLife;
+      },
+      logIn: function (provider, email, password) {
+        if (provider === email) {
+          auth.login('password', {
+            email: '<email@domain.com>',
+            password: '<password>'
+          });
+        }
+        else {auth.login(provider);}
+      },
+      loggedIn: function () {
+        if (user) {return true;}
+        else {return false;}
+      },
+      logOut: function () {
+        user = null;
+        auth.logout();
       }
     };
   });
