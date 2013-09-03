89d54a7dd2ed2523ca4d398a4478910903edd737
Poof Young <poof@coding4kicks.com>
2013-09-02 21:58:11 -0700

    Add beginning email and password check.  Need better validation and notification.

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -3,7 +3,14 @@
 angular.module('b4cmApp')
   .controller('SignupCtrl', function ($scope, user) {
     $scope.signup = function () {
-      alert('here');
+      if (typeof $scope.email === 'undefined' ||
+          typeof $scope.password === 'undefined') {
+        alert('Must enter valid password and email');
+      }
+      else {
+
+        alert('sign up');
+      }
     };
     $scope.signin = function () {
 
