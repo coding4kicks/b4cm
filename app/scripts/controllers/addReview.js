ce62f389bfa3e50930f1519514801d9f86571c3c
Poof Young <poof@coding4kicks.com>
2013-09-04 20:03:17 -0700

    Code clean and comments.

--- app/scripts/controllers/addReview.js
+++ app/scripts/controllers/addReview.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('b4cmApp')
+
+  /**
+   * @name Add Review Controller
+   * @controller
+   *
+   * @description Allows a user to add a review.  The data is denormalized.  
+   *              The review is saved both to its associated spot and to the user's data.
+   */ 
   .controller('AddReviewCtrl', function ($scope, $routeParams, spot, user) {
 
     var author = {},
@@ -9,11 +17,10 @@ angular.module('b4cmApp')
 
     $scope.RATINGS = [{'label': 1}, {'label': 2}, {'label': 3}, {'label': 4}, {'label': 5}];
     $scope.rating = $scope.RATINGS[2];
-  
     review.type = {'food': 0, 'study': 0, 'social': 0};
 
+    // Retreive the spot associated with this review.
     spot.get($routeParams.spotId).then(function(spot_data) {
-      console.log('hereeee');
       $scope.spot = spot_data;
       review.spotId = spot_data.id;
       additionalInfo.review_count = spot_data.review_count;
@@ -25,12 +32,12 @@ angular.module('b4cmApp')
      * @name addReview
      * @function
      *
-     * @description Adds a review to a spot
+     * @description Adds a review to a spot. Also save the review to the user's object
      */     
     $scope.addReview = function() {
 
       if (user.loggedIn()){
-        var curUser = user.getInfo()
+        var curUser = user.getInfo();
         author.id = curUser.provider + '/' + curUser.id,
         author.name = curUser.displayName, 
         author.pic = curUser.gravatar
@@ -47,28 +54,11 @@ angular.module('b4cmApp')
           if ($scope.food) {review.type.food = 1};
           if ($scope.study) {review.type.study = 1};
           if ($scope.social) {review.type.social = 1};
-          //spot.addReview(review, $routeParams.spotId).then(function(error) {
-          //  if (!error) {
-          //    user.addReview(review).then(function(error) {
-          //      if (!error) {
-          //        // redirect
-          //      }
-          //      else {console.log(error);}
-          //    });
-          //  }
-          //  else {console.log(error);}
-          //});
-          console.log(review);
           spot.addReview(review, $routeParams.spotId, additionalInfo);
           user.addReview(review, $routeParams.spotId)
-          // also add to user *******
         }
-        else {
-          alert("Must enter at least 1 type and a writeup.");
-        }
+        else {alert("Must enter at least 1 type and a writeup.");}
       }
-      else {
-        alert("Must be logged in to add a review");
-      }
+      else {alert("Must be logged in to add a review");}
     };
   });
