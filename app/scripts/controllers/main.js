a059429542c9c59cf8e0bff2750c611b93402694
Poof Young <poof@coding4kicks.com>
2013-09-05 19:54:19 -0700

    Added modal content.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,6 @@
 angular.module('b4cmApp')
   .controller('MainCtrl', function ($scope, $rootScope, $location, $modal) {
 
-   // $rootScope.searchLocation = $scope.searchLocation;
-
-   // $scope.findListings = function() {
-   //   $location.path("/listings");
-   //   // http://www.yearofmoo.com/2012/10/ ... apply-digest-and-phase
-   //   if(!$scope.$$phase) { $scope.$apply(); }
-   // };
-
     $scope.addWatch = function() {
       $location.path("/addWatch");
       // http://www.yearofmoo.com/2012/10/ ... apply-digest-and-phase
