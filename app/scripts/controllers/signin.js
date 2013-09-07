b09c842be451fcf63f52a1de7970f3d28dc8ce1a
Poof Young <poof@coding4kicks.com>
2013-09-06 21:22:22 -0700

    Remove stupid sign in alert.

--- app/scripts/controllers/signin.js
+++ app/scripts/controllers/signin.js
@@ -3,7 +3,6 @@
 angular.module('b4cmApp')
   .controller('SigninCtrl', function ($scope, user) {
     $scope.signIn = function (provider) {
-      alert('sign in: ', provider);
       user.logIn(provider, $scope.email, $scope.password);
     };
   });
