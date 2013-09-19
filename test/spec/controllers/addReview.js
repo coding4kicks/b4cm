3452ea05dc2bbad703412de3dbd79546a23c445f
Poof Young <poof@coding4kicks.com>
2013-09-19 15:14:36 -0700

    Test user logged in for addReview.

--- test/spec/controllers/addReview.js
+++ test/spec/controllers/addReview.js
@@ -6,17 +6,44 @@ describe('Controller: AddReviewCtrl', function () {
   beforeEach(module('b4cmApp'));
 
   var AddReviewCtrl,
-    scope;
+      scope,
+      window,
+      _user,
+      _spot,
+      promise;
+
+  // Instantiate user and spot services
+  beforeEach(module(function($provide) {
+    $provide.provider('user', function() {
+      this.$get = function() {
+        return {loggedIn: jasmine.createSpy('loggedIn').andReturn(false)};
+       };      
+    });
+    $provide.provider('spot', function() {
+      this.$get = function() {
+        return {};
+       };      
+    });
+  }));
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $window, $q, user, spot) {
     scope = $rootScope.$new();
+    window = $window;
+    _user = user;
+    _spot = spot;
+    promise = $q.defer().promise;
+    _spot.get = jasmine.createSpy('get').andReturn(promise);
     AddReviewCtrl = $controller('AddReviewCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-
+  it('addReview requires login', function () {
+    var userText = 'Must be logged in to add a review';
+    window.alert = jasmine.createSpy();
+    expect(window.alert).not.toHaveBeenCalled();
+    scope.addReview();
+    expect(window.alert).toHaveBeenCalledWith(userText);
   });
 });
