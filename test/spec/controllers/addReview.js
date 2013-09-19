cb3bc821e397a3c329e0c8b5ee486ae0489e1e60
Poof Young <poof@coding4kicks.com>
2013-09-19 15:25:51 -0700

    Test addReview adds a review.

--- test/spec/controllers/addReview.js
+++ test/spec/controllers/addReview.js
@@ -40,11 +40,13 @@ describe('Controller: AddReviewCtrl', function () {
   }));
 
   it('addReview requires login', function () {
-    var userText = 'Must be logged in to add a review';
+    var alertText = 'Must be logged in to add a review';
     window.alert = jasmine.createSpy();
     expect(window.alert).not.toHaveBeenCalled();
+    expect(_user.loggedIn).not.toHaveBeenCalled();
     scope.addReview();
-    expect(window.alert).toHaveBeenCalledWith(userText);
+    expect(window.alert).toHaveBeenCalledWith(alertText);
+    expect(_user.loggedIn).toHaveBeenCalled();
   });
 
   it('addReview requires type and writeup', function () {
@@ -54,8 +56,32 @@ describe('Controller: AddReviewCtrl', function () {
     _user.getInfo = jasmine.createSpy('getInfo').andReturn(
       {'provider': 'password', 'id': 1, 'display_name': 'Test', 'gravatar': 'fakeurl'});
     expect(window.alert).not.toHaveBeenCalled();
+    expect(_user.loggedIn).not.toHaveBeenCalled();
+    expect(_user.getInfo).not.toHaveBeenCalled();
     scope.addReview();
     expect(window.alert).toHaveBeenCalledWith(alertText);
+    expect(_user.loggedIn).toHaveBeenCalled();
+    expect(_user.getInfo).toHaveBeenCalled();
+  });
+
+  it('addReview adds a review', function () {
+    var alertText = 'Must enter at least 1 type and a writeup.';
+    window.alert = jasmine.createSpy();
+    _user.loggedIn = jasmine.createSpy('loggedIn').andReturn(true);
+    _user.getInfo = jasmine.createSpy('getInfo').andReturn(
+      {'provider': 'password', 'id': 1, 'display_name': 'Test', 'gravatar': 'fakeurl'});
+    _spot.addReview = jasmine.createSpy('addReview');
+    _user.addReview = jasmine.createSpy('addReview');
+    scope.food = true;
+    scope.writeup = 'Something to say.';
+    expect(window.alert).not.toHaveBeenCalled();
+    expect(_spot.addReview).not.toHaveBeenCalled();
+    expect(_user.addReview).not.toHaveBeenCalled();
+    scope.addReview();
+    expect(window.alert).not.toHaveBeenCalled();
+    // TODO: Check called with correct parameters - issue is the time changes
+    expect(_spot.addReview).toHaveBeenCalled();
+    expect(_user.addReview).toHaveBeenCalled();
   });
 
 });
