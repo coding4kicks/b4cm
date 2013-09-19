95429f8bf4a37928e210ec175454a8932b25ddfa
Poof Young <poof@coding4kicks.com>
2013-09-18 20:17:16 -0700

    Test addHours.

--- test/spec/controllers/addSpot.js
+++ test/spec/controllers/addSpot.js
@@ -16,6 +16,26 @@ describe('Controller: AddSpotCtrl', function () {
     });
   }));
 
-  it('expect nothing', function () { 
+  it('addHours correctly adds hours and incrments day', function () { 
+    expect(scope.business_hours.length).toBe(0);
+    scope.openDay = {'label': 'Friday'};
+    scope.openHour = {'label': '8:00', 'hour': 8, 'minutes': 0}
+    scope.openMeridiem = {'label': 'am'};
+    scope.closeDay = {'label': 'Friday'};
+    scope.closeHour = {'label': '8:00', 'hour': 8, 'minutes': 0}
+    scope.closeMeridiem = {'label': 'am'};
+    var oldOpen = scope.openDay,
+        oldClose = scope.closeDay;
+    scope.addHours();
+    expect(scope.business_hours.length).toBe(1);
+    expect(scope.business_hours[0].open_day).toBe(oldOpen);
+    expect(scope.business_hours[0].open_hour).toBe(scope.openHour);
+    expect(scope.business_hours[0].open_meridiem).toBe(scope.openMeridiem);
+    expect(scope.business_hours[0].close_day).toBe(oldClose);
+    expect(scope.business_hours[0].close_hour).toBe(scope.closeHour);
+    expect(scope.business_hours[0].close_meridiem).toBe(scope.closeMeridiem);
+    expect(scope.openDay.label).toBe('Saturday');
+    expect(scope.closeDay.label).toBe('Saturday');
+    console.log(scope.business_hours);
   });
 });
