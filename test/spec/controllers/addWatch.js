dd069da6a099be861bf6f0dd9fe94d9473bb9ffd
Poof Young <poof@coding4kicks.com>
2013-09-19 16:53:22 -0700

    Set up test for addWatch, but still failing since can't return spot_data.  Need to just make spotObj a scope variable so can set.

--- test/spec/controllers/addWatch.js
+++ test/spec/controllers/addWatch.js
@@ -49,4 +49,31 @@ describe('Controller: AddWatchCtrl', function () {
     expect(_user.loggedIn).toHaveBeenCalled();
   });
 
+  it('addWatch requires a status', function () {
+    var alertText = 'Please choose a crowd status.';
+    window.alert = jasmine.createSpy();
+    _user.loggedIn = jasmine.createSpy('loggedIn').andReturn(true);
+    _user.getInfo = jasmine.createSpy('getInfo').andReturn(
+      {'provider': 'password', 'id': 1, 'display_name': 'Test', 'gravatar': 'fakeurl'});
+    expect(window.alert).not.toHaveBeenCalled();
+    expect(_user.loggedIn).not.toHaveBeenCalled();
+    expect(_user.getInfo).not.toHaveBeenCalled();
+    scope.$digest();
+    scope.addWatch();
+    expect(window.alert).toHaveBeenCalledWith(alertText);
+    expect(_user.loggedIn).toHaveBeenCalled();
+    expect(_user.getInfo).toHaveBeenCalled();
+  });
+
+  function _setValidScope(scope) {
+    scope.startDay.label = 'Friday'; 
+    scope.startHour.label = '8';
+    scope.startMeridiem.label = 'am'
+    scope.stopDay.label = 'Friday';
+    scope.stopHour.label = '10';
+    scope.stopMeridiem.label = 'am';
+  }
+
 });
+
+
