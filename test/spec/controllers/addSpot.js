df77b2f0cf072d98e60eb744b61df2dfb420d005
Poof Young <poof@coding4kicks.com>
2013-09-18 20:37:54 -0700

    Test yelpIdHelp and that addSpot alerts when invalid form.

--- test/spec/controllers/addSpot.js
+++ test/spec/controllers/addSpot.js
@@ -6,17 +6,19 @@ describe('Controller: AddSpotCtrl', function () {
   beforeEach(module('b4cmApp'));
 
   var AddSpotCtrl,
-    scope;
+      scope,
+      window;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $window) {
+    window = $window;
     scope = $rootScope.$new();
     AddSpotCtrl = $controller('AddSpotCtrl', {
       $scope: scope
     });
   }));
 
-  it('addHours correctly adds hours and incrments day', function () { 
+  it('addHours adds hours and incrments the day', function () { 
     expect(scope.business_hours.length).toBe(0);
     scope.openDay = {'label': 'Friday'};
     scope.openHour = {'label': '8:00', 'hour': 8, 'minutes': 0}
@@ -36,14 +38,27 @@ describe('Controller: AddSpotCtrl', function () {
     expect(scope.business_hours[0].close_meridiem).toBe(scope.closeMeridiem);
     expect(scope.openDay.label).toBe('Saturday');
     expect(scope.closeDay.label).toBe('Saturday');
-    console.log(scope.business_hours);
   });
 
-  it('deleteHours correctly removes a time', function () { 
+  it('deleteHours removes a time', function () { 
     scope.business_hours = ['dummyData'];
     expect(scope.business_hours.length).toBe(1);
     scope.deleteHours();
     expect(scope.business_hours.length).toBe(0);
   });
+
+  it('yelpIdHelp toggles', function () { 
+    scope.yelpHelpShow = false;
+    scope.yelpIdHelp();
+    expect(scope.yelpHelpShow).toBe(true);
+  });
+
+  it('addSpot checks required form elements', function () { 
+    window.alert = jasmine.createSpy();
+    expect(window.alert).not.toHaveBeenCalled();
+    scope.addSpot();
+    expect(window.alert).toHaveBeenCalled();
+  });
+
 
 });
