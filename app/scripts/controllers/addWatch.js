364bc05316b3ef14c7111ee1a114e8974f2cbfab
Poof Young <poof@coding4kicks.com>
2013-09-20 12:30:07 -0700

    Lint addWatch.  Use util.dayToNum vice global _dayToNum

--- app/scripts/controllers/addWatch.js
+++ app/scripts/controllers/addWatch.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global alert */
+
 angular.module('b4cmApp')
 
   /**
@@ -7,40 +9,40 @@ angular.module('b4cmApp')
    * @controller
    *
    * @description Adds a Crowd Watch report to a Spot's crowdfactor.
-   */ 
+   */
   .controller('AddWatchCtrl', function ($scope, $routeParams, $location, spot, user, util) {
 
     var watch = {},
         //spotObj = {},
         additionalInfo = {},
-        current_date = new Date(),
-        future_date = new Date(current_date.getTime() + 60 * 60 * 1000),
-        current_day = current_date.getDay(),
-        future_day = future_date.getDay(),
-        current_hour = current_date.getHours() % 12,
-        future_hour = future_date.getHours() % 12,
-        current_meridiem = (current_date.getHours() < 12) ? '0' : '1',
-        future_meridiem = (future_date.getHours() < 12) ? '0' : '1';
+        currentDate = new Date(),
+        futureDate = new Date(currentDate.getTime() + 60 * 60 * 1000),
+        currentDay = currentDate.getDay(),
+        futureDay = futureDate.getDay(),
+        currentHour = currentDate.getHours() % 12,
+        futureHour = futureDate.getHours() % 12,
+        currentMeridiem = (currentDate.getHours() < 12) ? '0' : '1',
+        futureMeridiem = (futureDate.getHours() < 12) ? '0' : '1';
 
     $scope.spotObj = {}; // Scope reference only used for testing.
-    $scope.WEEKDAYS = [{'label': 'Sunday'}, {'label': 'Monday'}, {'label': 'Tuesday'}, 
-                       {'label': 'Wednesday'}, {'label': 'Thursday'}, {'label': 'Friday'}, 
+    $scope.WEEKDAYS = [{'label': 'Sunday'}, {'label': 'Monday'}, {'label': 'Tuesday'},
+                       {'label': 'Wednesday'}, {'label': 'Thursday'}, {'label': 'Friday'},
                        {'label': 'Saturday'}],
-    $scope.HOURS = [{'label': '12'}, {'label': '1'}, {'label': '2'}, {'label': '3'}, 
-                    {'label': '4'}, {'label': '5'}, {'label': '6'}, {'label': '7'}, 
+    $scope.HOURS = [{'label': '12'}, {'label': '1'}, {'label': '2'}, {'label': '3'},
+                    {'label': '4'}, {'label': '5'}, {'label': '6'}, {'label': '7'},
                     {'label': '8'}, {'label': '9'}, {'label': '10'}, {'label': '11'}],
     $scope.MERIDIEMS = [{'label': 'am'}, {'label': 'pm'}];
 
-    $scope.startDay = $scope.WEEKDAYS[current_day];
-    $scope.stopDay = $scope.WEEKDAYS[future_day];
-    $scope.startHour = $scope.HOURS[current_hour];
-    $scope.stopHour = $scope.HOURS[future_hour];
-    $scope.startMeridiem = $scope.MERIDIEMS[current_meridiem];
-    $scope.stopMeridiem = $scope.MERIDIEMS[future_meridiem];
+    $scope.startDay = $scope.WEEKDAYS[currentDay];
+    $scope.stopDay = $scope.WEEKDAYS[futureDay];
+    $scope.startHour = $scope.HOURS[currentHour];
+    $scope.stopHour = $scope.HOURS[futureHour];
+    $scope.startMeridiem = $scope.MERIDIEMS[currentMeridiem];
+    $scope.stopMeridiem = $scope.MERIDIEMS[futureMeridiem];
 
     // Retreive the spot associated with this review.
-    spot.get($routeParams.spotId).then(function(spot_data) {
-      $scope.spotObj = spot_data;
+    spot.get($routeParams.spotId).then(function(spotData) {
+      $scope.spotObj = spotData;
     });
 
     /**
@@ -48,15 +50,16 @@ angular.module('b4cmApp')
      * @function
      *
      * @description Adds a crowd watch to a spot
-     */     
+     */
     $scope.addWatch = function() {
       if (user.loggedIn()){
-        var start = {'day': _dayToNum($scope.startDay.label), 
-                     'hour': parseInt($scope.startHour.label), 
+        var start = {'day': util.dayToNum($scope.startDay.label),
+                     'hour': parseInt($scope.startHour.label, 10),
                      'meridiem': $scope.startMeridiem.label},
-            stop = {'day': _dayToNum($scope.stopDay.label), 
-                    'hour': parseInt($scope.stopHour.label), 
-                    'meridiem': $scope.stopMeridiem.label}
+            stop = {'day': util.dayToNum($scope.stopDay.label),
+                    'hour': parseInt($scope.stopHour.label, 10),
+                    'meridiem': $scope.stopMeridiem.label};
+        /* jshint camelcase: false */
         watch.cf_status = $scope.cf_status;
         watch.time = _calculateWatchTimes(start, stop, $scope.spotObj);
         watch.comment = $scope.watchComment;
@@ -64,9 +67,10 @@ angular.module('b4cmApp')
         if (typeof watch.cf_status === 'undefined') {alert('Please choose a crowd status.');}
         else {
           spot.addWatch(watch, $routeParams.spotId, $scope.spotObj.crowdfactor.watch_count);
+          /* jshint camelcase: true */
           user.incrementWatchCount();
           alert('Crowd watch added.');
-          
+
           if ($scope.multipleWatches) {
             alert('multiples');
           }
@@ -87,6 +91,10 @@ angular.module('b4cmApp')
 /***************
  * HELPER FUNCS
  ***************/
+/* TODO: Move to utility */
+/* jshint -W098 */
+/* jshint -W003 */
+
 
 /**
  * @name _calculateWatchTimes
@@ -99,7 +107,7 @@ angular.module('b4cmApp')
  * @param {object} start The start time. Props: day (int), hour (int), meridiem (string)
  * @param {object} stop The stop time. Props: day (int), hour (int), meridiem (string)
  * @return {array} An array of watch time lables i.e {'day': 'monday', 'hour': '11pm'}
- */ 
+ */
 function _calculateWatchTimes(start, stop, spotObj) {
   var times = [],
       day = spotObj.crowdfactor.day,
@@ -119,7 +127,7 @@ function _calculateWatchTimes(start, stop, spotObj) {
     watch.count = day[watch.day][watch.hour].count;
     watch.score = day[watch.day][watch.hour].score;
     // only add if not closed
-    if (day[watch.day][watch.hour].count !== -1) {times.push(watch)};
+    if (day[watch.day][watch.hour].count !== -1) {times.push(watch);}
 
     // Increment current time
     current.hour = current.hour + 1;
@@ -135,6 +143,6 @@ function _calculateWatchTimes(start, stop, spotObj) {
     }
     else if (current.hour === 13) {current.hour = 1;}
   }
-  return times
+  return times;
 }
 
