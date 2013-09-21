6f0acdc2c94103f1cabacf0c49bcf75099d53848
Poof Young <poof@coding4kicks.com>
2013-09-21 15:38:52 -0700

    Start addMultiple with baseline of code from addWatch.

--- app/scripts/controllers/addMultipleWatches.js
+++ app/scripts/controllers/addMultipleWatches.js
@@ -1,10 +1,174 @@
 'use strict';
 
+/* global alert */
+
 angular.module('b4cmApp')
-  .controller('AddMultipleWatchesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  /**
+   * @name Add Watch Controller
+   * @controller
+   *
+   * @description Adds a Crowd Watch report to a Spot's crowdfactor.
+   */
+  .controller('AddMultipleWatchesCtrl', function ($scope, $routeParams, $location, spot, user, util) {
+
+    var watch = {},
+        //spotObj = {},
+        additionalInfo = {},
+        currentDate = new Date(),
+        futureDate = new Date(currentDate.getTime() + 60 * 60 * 1000),
+        currentDay = currentDate.getDay(),
+        futureDay = futureDate.getDay(),
+        currentHour = currentDate.getHours() % 12,
+        futureHour = futureDate.getHours() % 12,
+        currentMeridiem = (currentDate.getHours() < 12) ? '0' : '1',
+        futureMeridiem = (futureDate.getHours() < 12) ? '0' : '1';
+
+    $scope.spotObj = {}; // Scope reference only used for testing.
+    $scope.WEEKDAYS = [{'label': 'Sunday'}, {'label': 'Monday'}, {'label': 'Tuesday'},
+                       {'label': 'Wednesday'}, {'label': 'Thursday'}, {'label': 'Friday'},
+                       {'label': 'Saturday'}],
+    $scope.HOURS = [{'label': '12'}, {'label': '1'}, {'label': '2'}, {'label': '3'},
+                    {'label': '4'}, {'label': '5'}, {'label': '6'}, {'label': '7'},
+                    {'label': '8'}, {'label': '9'}, {'label': '10'}, {'label': '11'}],
+    $scope.MERIDIEMS = [{'label': 'am'}, {'label': 'pm'}];
+
+    $scope.startDay = $scope.WEEKDAYS[currentDay];
+    $scope.stopDay = $scope.WEEKDAYS[futureDay];
+    $scope.startHour = $scope.HOURS[currentHour];
+    $scope.stopHour = $scope.HOURS[futureHour];
+    $scope.startMeridiem = $scope.MERIDIEMS[currentMeridiem];
+    $scope.stopMeridiem = $scope.MERIDIEMS[futureMeridiem];
+
+    // Retreive the spot associated with this review.
+    spot.get($routeParams.spotId).then(function(spotData) {
+      $scope.spotObj = spotData;
+      $scope.name = spotData.name;
+      $scope.address = spotData.location.address;
+      $scope.city = spotData.location.city;
+    });
+
+    /**
+     * @name addWatch
+     * @function
+     *
+     * @description Adds a crowd watch to a spot
+     */
+    $scope.addWatch = function() {
+      if (user.loggedIn()){
+        var start = {'day': util.dayToNum($scope.startDay.label),
+                     'hour': parseInt($scope.startHour.label, 10),
+                     'meridiem': $scope.startMeridiem.label},
+            stop = {'day': util.dayToNum($scope.stopDay.label),
+                    'hour': parseInt($scope.stopHour.label, 10),
+                    'meridiem': $scope.stopMeridiem.label};
+        /* jshint camelcase: false */
+        watch.cf_status = $scope.cf_status;
+        watch.time = _calculateWatchTimes(start, stop, $scope.spotObj);
+        watch.comment = $scope.watchComment;
+        watch.user = user.getInfo().display_name;
+
+        if (typeof watch.cf_status === 'undefined') {alert('Please choose a crowd status.');}
+        else if (watch.time.length > 24) {alert('Watches must be for less than a 24 hour period.');}
+        else {
+          spot.addWatch(watch, $routeParams.spotId, $scope.spotObj.crowdfactor.watch_count);
+          /* jshint camelcase: true */
+          user.incrementWatchCount();
+          alert('Crowd watch added.');
+
+          if ($scope.multipleWatches) {
+            alert('multiples');
+          }
+          else {
+            alert('redirect');
+          }
+        }
+      }
+      else {
+        alert('Must be logged in to add a watch');
+        $location.path('/signin');
+        util.safeApply($scope);
+      }
+    };
+
+    /**
+     * @name addMultiple
+     * @function
+     *
+     * @description Redirects to add-multiple-watches-page.
+     */ 
+    $scope.addMultiple = function() {
+      console.log('here');
+      if (user.loggedIn()){
+        $location.path("/addMultipleWatches/" + $scope.spotObj.id);
+        util.safeApply($scope);
+      }
+      else {
+        alert("Must be logged in to add watches.");
+        $location.path("/signin/");
+        util.safeApply($scope);
+      }
+    };
+
+
   });
+
+/***************
+ * HELPER FUNCS
+ ***************/
+/* TODO: Move to utility */
+/* jshint -W098 */
+/* jshint -W003 */
+
+
+/**
+ * @name _calculateWatchTimes
+ * @function
+ *
+ * @description Given a start and stop times, creates an array of time lables
+ *              used as keys for updating a spot's crowdseer.  Also determines the total 
+ *              score and count for the time periods.  Also checks time is not closed
+ *              and score for the 
+ * @param {object} start The start time. Props: day (int), hour (int), meridiem (string)
+ * @param {object} stop The stop time. Props: day (int), hour (int), meridiem (string)
+ * @return {array} An array of watch time lables i.e {'day': 'monday', 'hour': '11pm'}
+ */
+function _calculateWatchTimes(start, stop, spotObj) {
+  var times = [],
+      day = spotObj.crowdfactor.day,
+      current = {'day': start.day, 'hour': start.hour, 'meridiem': start.meridiem},
+      WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  while (current.day !== stop.day ||
+         current.hour !== stop.hour ||
+         current.meridiem !== stop.meridiem) {
+
+    // Add watch to times
+    var watch = {};
+    var i = 0;
+    i = i + 1;
+    watch.day = WEEKDAYS[current.day].toLowerCase();
+    watch.hour = current.hour + current.meridiem;
+    watch.count = day[watch.day][watch.hour].count;
+    watch.score = day[watch.day][watch.hour].score;
+    // only add if not closed
+    if (day[watch.day][watch.hour].count !== -1) {times.push(watch);}
+
+    // Increment current time
+    current.hour = current.hour + 1;
+    if (current.hour === 12) {
+      if (current.meridiem === 'pm') {
+        current.day = current.day + 1;
+        if (current.day === 7) {current.day = 0;}
+        current.meridiem = 'am';
+      }
+      else {
+        current.meridiem = 'pm';
+      }
+    }
+    else if (current.hour === 13) {current.hour = 1;}
+  }
+  return times;
+}
+
+
--- test/spec/controllers/addMultipleWatches.js
+++ test/spec/controllers/addMultipleWatches.js
@@ -6,17 +6,150 @@ describe('Controller: AddMultipleWatchesCtrl', function () {
   beforeEach(module('b4cmApp'));
 
   var AddMultipleWatchesCtrl,
-    scope;
+      scope,
+      window,
+      _user,
+      _spot,
+      promise;
+
+  // Instantiate user and spot services
+  beforeEach(module(function($provide) {
+    $provide.provider('user', function() {
+      this.$get = function() {
+        return {loggedIn: jasmine.createSpy('loggedIn').andReturn(false)};
+       };      
+    });
+    $provide.provider('spot', function() {
+      this.$get = function() {
+        return {};
+       };      
+    });
+  }));
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $window, $q, user, spot) {
     scope = $rootScope.$new();
+    window = $window;
+    _user = user;
+    _spot = spot;
+    promise = $q.defer().promise;
+    _spot.get = jasmine.createSpy('get').andReturn(promise);
     AddMultipleWatchesCtrl = $controller('AddMultipleWatchesCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('addWatch requires login', function () {
+    var alertText = 'Must be logged in to add a watch';
+    window.alert = jasmine.createSpy();
+    expect(window.alert).not.toHaveBeenCalled();
+    expect(_user.loggedIn).not.toHaveBeenCalled();
+    scope.addWatch();
+    expect(window.alert).toHaveBeenCalledWith(alertText);
+    expect(_user.loggedIn).toHaveBeenCalled();
   });
+
+  it('addWatch requires a status', function () {
+    var alertText = 'Please choose a crowd status.';
+    window.alert = jasmine.createSpy();
+    _user.loggedIn = jasmine.createSpy('loggedIn').andReturn(true);
+    _user.getInfo = jasmine.createSpy('getInfo').andReturn(
+      {'provider': 'password', 'id': 1, 'display_name': 'Test', 'gravatar': 'fakeurl'});
+    scope.spotObj = {'crowdfactor': _fullCrowdGraph()};
+    expect(window.alert).not.toHaveBeenCalled();
+    expect(_user.loggedIn).not.toHaveBeenCalled();
+    expect(_user.getInfo).not.toHaveBeenCalled();
+    scope.addWatch();
+    expect(window.alert).toHaveBeenCalledWith(alertText);
+    expect(_user.loggedIn).toHaveBeenCalled();
+    expect(_user.getInfo).toHaveBeenCalled();
+  });
+
+  it('addWatch period must be less than 24 hours', function () {
+    var alertText = 'Watches must be for less than a 24 hour period.';
+    window.alert = jasmine.createSpy();
+    _user.loggedIn = jasmine.createSpy('loggedIn').andReturn(true);
+    _user.getInfo = jasmine.createSpy('getInfo').andReturn(
+      {'provider': 'password', 'id': 1, 'display_name': 'Test', 'gravatar': 'fakeurl'});
+    scope.spotObj = {'crowdfactor': _fullCrowdGraph()};
+    scope.cf_status = 'empty';
+    _setInvalidScope(scope);
+    expect(window.alert).not.toHaveBeenCalled();
+    scope.addWatch();
+    expect(window.alert).toHaveBeenCalledWith(alertText);
+  });
+
+  it('addWatch adds a watch', function () {
+    var alertText = 'Crowd watch added.',
+        watchObj = {cf_status: 'empty', 
+                    time: [{day: 'friday', hour: '8am', count: 1, score: 3 }, 
+                           {day: 'friday', hour: '9am', count: 1, score: 3}], 
+                    comment: undefined, user: 'Test'};
+    
+    window.alert = jasmine.createSpy();
+    _user.loggedIn = jasmine.createSpy('loggedIn').andReturn(true);
+    _user.getInfo = jasmine.createSpy('getInfo').andReturn(
+      {'provider': 'password', 'id': 1, 'display_name': 'Test', 'gravatar': 'fakeurl'});
+    scope.spotObj = {'crowdfactor': _fullCrowdGraph()};
+    scope.cf_status = 'empty';
+    _setValidScope(scope);
+    _spot.addWatch = jasmine.createSpy('addWatch');
+    _user.incrementWatchCount = jasmine.createSpy('incrementWatchCount');
+    expect(window.alert).not.toHaveBeenCalled();
+    expect(_user.incrementWatchCount).not.toHaveBeenCalled();
+    expect(_spot.addWatch).not.toHaveBeenCalled();
+    scope.addWatch();
+    expect(window.alert).toHaveBeenCalledWith(alertText);
+    expect(_user.incrementWatchCount).toHaveBeenCalled();
+    expect(_spot.addWatch).toHaveBeenCalledWith(watchObj, undefined, 0);
+  });
+
+  function _setValidScope(scope) {
+    scope.startDay = {'label': 'Friday'}; 
+    scope.startHour = {'label': 8};
+    scope.startMeridiem = {'label': 'am'};
+    scope.stopDay = {'label': 'Friday'};
+    scope.stopHour = {'label': 10};
+    scope.stopMeridiem = {'label': 'am'};
+  }
+
+  function _setInvalidScope(scope) {
+    scope.startDay = {'label': 'Friday'}; 
+    scope.startHour = {'label': 8};
+    scope.startMeridiem = {'label': 'am'};
+    scope.stopDay = {'label': 'Saturday'};
+    scope.stopHour = {'label': 11};
+    scope.stopMeridiem = {'label': 'am'};
+  }
+
+  function _fullCrowdGraph() {
+    var crowdgraph = {},
+        WEEKDAYS = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
+        BLOCK_HOURS = {'morning': ['5am', '6am', '7am', '8am', '9am', '10am'],
+                       'afternoon': ['11am', '12pm', '1pm', '2pm', '3pm', '4pm'],
+                       'evening': ['5pm', '6pm', '7pm', '8pm', '9pm', '10pm'],
+                       'latenight': ['11pm', '12am', '1am', '2am', '3am', '4am']
+                      };
+    crowdgraph.watch_count = 0;
+    crowdgraph.most_recent = {'time': 0, 'score': 0};
+    crowdgraph.blocks = {};
+    crowdgraph.day = {};
+    
+    // Construct crowdgraph with all closed
+    WEEKDAYS.forEach(function(day) {
+      crowdgraph.day[day] = {};
+      for (var i = 1; i <= 12; i++) {
+        var statusAm = {'count': 1, 'score': 3},
+            statusPm = {'count': 1, 'score': 3};
+        crowdgraph.day[day][i + 'am'] = statusAm;
+        crowdgraph.day[day][i + 'pm'] = statusPm;
+      }
+    });
+
+    return crowdgraph
+  }
+
 });
+
+
+
