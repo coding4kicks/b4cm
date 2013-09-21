6add2642780f2c90891690bac19f1a18ec44f099
Poof Young <poof@coding4kicks.com>
2013-09-21 13:51:29 -0700

    Move a copy of initGoogleMaps to util service.

--- app/scripts/services/util.js
+++ app/scripts/services/util.js
@@ -54,6 +54,69 @@ angular.module('b4cmApp')
       },
 
       /**
+       * @name _initializeGoogleMaps
+       * @procedure
+       *
+       * @description Initialize parameters for google maps directive for the spot page.
+       * @params {object} $scope Controller's scope.
+       * @returns {nothing} Procedure has side effects on scope.
+       */
+      initializeGoogleMaps: function($scope, position, spots, zoom) {
+      
+        // TODO: Need to make sure center and zoom encompass all the locations.  
+        //       Should probably just have zoom level and search radius be equal. How?
+      
+        // Enable the new Google Maps visuals until it gets enabled by default.
+        // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
+        // Add test for existance so doesn't blow up unit tests
+        console.log('currently initializing google maps');
+        if (typeof google !== 'undefined') {
+          google.maps.visualRefresh = true;
+        }
+      
+        $scope.position = {
+          'coords': {
+            'latitude': position.latitude,
+            'longitude': position.longitude
+          }
+        };
+      
+        /** the initial center of the map */
+        $scope.centerProperty = {
+          'latitude': position.latitude,
+          'longitude': position.longitude
+        };
+      
+        /** the initial zoom level of the map */
+        $scope.zoomProperty = zoom || 14;
+      
+        /** list of markers to put in the map */
+        $scope.markersProperty = [];
+        for (var i = 0; i < spots.length; i++) {
+          var marker = {},
+              spot = spots[i],
+              url = '../images/marker-icon' + (i + 1) + '.png';
+          marker.latitude = spot.location.latitude;
+          marker.longitude = spot.location.longitude;
+          marker.infoWindow = spot.name;
+          marker.icon = {'url': url};
+          $scope.markersProperty.push(marker);
+        }
+      
+        // These 2 properties will be set when clicking on the map
+        $scope.clickedLatitudeProperty = null;
+        $scope.clickedLongitudeProperty = null;
+        $scope.eventsProperty = {
+          'click': function (mapModel, eventName, originalEventArgs) {
+            // 'this' is the directive's scope
+            $log.log('user defined event on map directive with scope', this);
+            $log.log('user defined event: ' + eventName, mapModel, originalEventArgs);
+          }
+        };
+        $scope.isMapElementHidden = false;
+      },
+
+      /**
        * @name safeApply
        * @procedure
        *
