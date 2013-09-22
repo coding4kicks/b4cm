b3c1ebbe05800b32e5e102f0e37e3003147724a1
Poof Young <poof@coding4kicks.com>
2013-09-21 18:31:36 -0700

    Relabel spotObj to spot in addWatchMultiple controller.

--- app/scripts/controllers/addMultipleWatches.js
+++ app/scripts/controllers/addMultipleWatches.js
@@ -13,7 +13,6 @@ angular.module('b4cmApp')
   .controller('AddMultipleWatchesCtrl', function ($scope, $routeParams, $location, spot, user, util) {
 
     var watch = {},
-        //spotObj = {},
         additionalInfo = {},
         currentDate = new Date(),
         futureDate = new Date(currentDate.getTime() + 60 * 60 * 1000),
@@ -24,7 +23,7 @@ angular.module('b4cmApp')
         currentMeridiem = (currentDate.getHours() < 12) ? '0' : '1',
         futureMeridiem = (futureDate.getHours() < 12) ? '0' : '1';
 
-    $scope.spotObj = {}; // Scope reference only used for testing.
+    $scope.spot = {}; // Scope reference only used for testing.
     $scope.WEEKDAYS = [{'label': 'Sunday'}, {'label': 'Monday'}, {'label': 'Tuesday'},
                        {'label': 'Wednesday'}, {'label': 'Thursday'}, {'label': 'Friday'},
                        {'label': 'Saturday'}, {'label': 'Weekdays'}, {'label': 'Weekends'}],
@@ -42,7 +41,7 @@ angular.module('b4cmApp')
 
     // Retreive the spot associated with this review.
     spot.get($routeParams.spotId).then(function(spotData) {
-      $scope.spotObj = spotData;
+      $scope.spot = spotData;
       $scope.name = spotData.name;
       $scope.address = spotData.location.address;
       $scope.city = spotData.location.city;
@@ -75,14 +74,14 @@ angular.module('b4cmApp')
         //            'meridiem': $scope.stopMeridiem.label};
         ///* jshint camelcase: false */
         //watch.cf_status = $scope.cf_status;
-        //watch.time = _calculateWatchTimes(start, stop, $scope.spotObj);
+        //watch.time = _calculateWatchTimes(start, stop, $scope.spot);
         //watch.comment = $scope.watchComment;
         //watch.user = user.getInfo().display_name;
 
         //if (typeof watch.cf_status === 'undefined') {alert('Please choose a crowd status.');}
         //else if (watch.time.length > 24) {alert('Watches must be for less than a 24 hour period.');}
         //else {
-        //  spot.addWatch(watch, $routeParams.spotId, $scope.spotObj.crowdfactor.watch_count);
+        //  spot.addWatch(watch, $routeParams.spotId, $scope.spot.crowdfactor.watch_count);
         //  /* jshint camelcase: true */
         //  user.incrementWatchCount();
         //  alert('Crowd watch added.');
@@ -125,9 +124,9 @@ angular.module('b4cmApp')
  * @param {object} stop The stop time. Props: day (int), hour (int), meridiem (string)
  * @return {array} An array of watch time lables i.e {'day': 'monday', 'hour': '11pm'}
  */
-function _calculateWatchTimes(start, stop, spotObj) {
+function _calculateWatchTimes(start, stop, spot) {
   var times = [],
-      day = spotObj.crowdfactor.day,
+      day = spot.crowdfactor.day,
       current = {'day': start.day, 'hour': start.hour, 'meridiem': start.meridiem},
       WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
