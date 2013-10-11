fb526fe3f5565df529a5e32d1872b21ee7078eae
Poof Young <poof@coding4kicks.com>
2013-10-11 13:43:58 -0700

    create twitter service

---
+++ app/scripts/services/twitter.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('b4cmApp')
+  .factory('twitter', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
